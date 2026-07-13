60b9b0b2bf994bb48630d1f64c724f83d1491a61
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2026-07-13 15:37:03 +0900

    P51 3.5

---
+++ chapter03/3_3/data/menuItems.js
@@ -0,0 +1,22 @@
+// メニュー項目の配列
+// JavaScriptの配列リテラル
+export default [
+  {
+    name: 'Broccoli Pie',
+    description: 'A green pie with an earthy crust',
+    translation: `素朴な風味のクラストを使った、緑色のパイ`,
+    cost: 12.99,
+  },
+  {
+    name: 'Eggplant Smoothie',
+    description: 'A purple shake with an earthy quake',
+    translation: `大地を揺るがすような力強さを秘めた、紫色のシェイク`,
+    cost: 5.99,
+  },
+  {
+    name: 'Watermelon Sushi',
+    description: 'A red roll with atmospheric sweetness',
+    translation: `情緒ある甘みを湛えた、赤いロールケーキ`,
+    cost: 8.99,
+  },
+];

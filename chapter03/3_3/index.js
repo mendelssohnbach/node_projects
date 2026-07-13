5a59eb082f31a6e8b4d95ee58b072254b207966b
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2026-07-13 16:58:32 +0900

    P53 発展課題を追加

--- chapter03/3_3/index.js
+++ chapter03/3_3/index.js
@@ -1,6 +1,8 @@
-import fastify from 'fastify';
+import Fastify from 'fastify';
+import menuItems from './data/menuItems.js';
+import operatingHours from './data/operatingHours.js';
 
-const app = fastify();
+const app = Fastify();
 const port = 3000;
 
 // ルート / でHTTP GETリクエストをリッスンするルートを登録
@@ -12,15 +14,20 @@ app.get('/', async (request, reply) => {
 });
 
 // /menu パスへのGETリクエストのためのルート
-app.get('/menu', async (Response, replay) => {
-  return 'TODO: Menu Page';
+app.get('/menu', async (request, reply) => {
+  reply.send(menuItems);
 });
 
 // /hours パスへのGETリクエストのためのルート
-app.get('/hours', async (Response, replay) => {
-  return 'TODO: Hours Page';
+app.get('/hours', async (request, reply) => {
+  reply.send(operatingHours);
 });
 
 // サーバを起動し、定義したポートにバインドする
 await app.listen({ port, host: '0.0.0.0' });
-console.log(`Web Service is listening at http://localhost:${port}`);
+console.log(`Web Server is listening at http://localhost:${port}`);
+
+// TODO //
+// ルートに簡単なエラー処理を書く
+// エラー処理をロギングに書き換える
+// メニューデータに id を追加を検討する

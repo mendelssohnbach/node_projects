4eefacb2380a06cfe564ec7485ef9cf9129c9a5a
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2026-07-13 15:18:31 +0900

    P50 3-4

--- chapter03/3_3/index.js
+++ chapter03/3_3/index.js
@@ -11,6 +11,16 @@ app.get('/', async (request, reply) => {
   return "Welcome to What's Fare is Fair!";
 });
 
+// /menu パスへのGETリクエストのためのルート
+app.get('/menu', async (Response, replay) => {
+  return 'TODO: Menu Page';
+});
+
+// /hours パスへのGETリクエストのためのルート
+app.get('/hours', async (Response, replay) => {
+  return 'TODO: Hours Page';
+});
+
 // サーバを起動し、定義したポートにバインドする
 await app.listen({ port, host: '0.0.0.0' });
 console.log(`Web Service is listening at http://localhost:${port}`);

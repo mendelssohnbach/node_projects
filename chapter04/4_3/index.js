6a22bfe9d4600e7ed147c0400e38497fef43dd09
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2026-07-16 15:01:56 +0900

    P70 4-3

--- chapter04/4_3/index.js
+++ chapter04/4_3/index.js
@@ -1,8 +1,6 @@
-import bcrypt from 'bcrypt';
+import promptModule from 'prompt-sync';
 
-const password = 'test1234';
-// 強度10の設定（ストレッチング回数）を使って暗号化
-// 毎回違う暗号化結果になるように、内部で自動生成されるランダムなデータで
-// 暗号計算を2の10乗回繰り返す
-const hash = bcrypt.hashSync(password, 10);
-console.log(`My hashed password is: ${hash}`);
+// prompt をインスタンス化し、同期的にプロンプト機能を利用
+const prompt = promptModule();
+// メモリ内ストレージを表すオブジェクト定義
+const mockDB = { passwords: {} };

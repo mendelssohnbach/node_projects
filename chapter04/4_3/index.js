4ea338cd0f26a03ad61cd3397258580f24dac4e6
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2026-07-17 06:45:01 +0900

    P71 4-6

--- chapter04/4_3/index.js
+++ chapter04/4_3/index.js
@@ -16,3 +16,30 @@ const saveNewPassword = (password) => {
 // プレーンテキストパスワードとハッシュ化パスワードを比較する関数
 // 入力されたパスワードとローカルデータベース内の値を比較する
 const compareHashedPassword = async (password) => await bcrypt.compare(password, mockDB.hash);
+
+const promptNewPassword = () => {
+  // 新しいマスターパスワードを入力するよう促す
+  const response = prompt('Enter a main password: ');
+  saveNewPassword(response);
+};
+
+const promptOldPassword = async () => {
+  // パスワーdがバリデーションされたかを追跡するフラグを定義
+  let verified = false;
+  while (!verified) {
+    // 既存のマスターパスワードを再入力するよう促す
+    const response = prompt('Enter your password: ');
+    // 入力パスワードを保存済ハッシュ化パスワードと比較
+    const result = await compareHashedPassword(response);
+    if (result) {
+      console.log('Password verified.');
+      // パスワードがバリデーションされたらバリデーションフラグを true に設定
+      verified = true;
+      // メニューを表示
+      showMenu();
+    } else {
+      // パスワードが正しくない場合は、エラーを表示し再試行する
+      console.log('Password incorrect. Try again.');
+    }
+  }
+};

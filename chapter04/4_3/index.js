cd24cb4e4a8e4c65b54cc6fdfade87f23e63bccd
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2026-07-16 15:07:49 +0900

    P70 4-4

--- chapter04/4_3/index.js
+++ chapter04/4_3/index.js
@@ -1,6 +1,14 @@
+import bcrypt from 'bcrypt';
 import promptModule from 'prompt-sync';
 
 // prompt をインスタンス化し、同期的にプロンプト機能を利用
 const prompt = promptModule();
 // メモリ内ストレージを表すオブジェクト定義
 const mockDB = { passwords: {} };
+
+const saveNewPassword = (password) => {
+  // プレーンテキストをハッシュ化し、データベース内の hash キーにセット
+  mockDB.hash = bcrypt.hashSycn(password, 10);
+  console.log('Main password has been set!');
+  showMenu();
+};

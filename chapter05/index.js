ac625e86777f57ece084a4a84743c7c2f4728518
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2026-07-21 07:44:10 +0900

    P99 5章練習問題

--- chapter05/index.js
+++ chapter05/index.js
@@ -14,6 +14,10 @@ const urls = [
   `https://www.budgetbytes.com/category/recipes/feed/`,
 ];
 
+// 5章 1-b
+// キーワードの入力を促す
+const keyword = prompt('Enter a keyword to filter feed items: ');
+
 // HTTPリクエストの処理をまとめる非同期関数
 const main = async () => {
   // RSSフィード項目を格納する空配列
@@ -36,16 +40,41 @@ const aggregate = (responses, feedItems) => {
   // 分割代入で items のみ取得
   for (let { items } of responses) {
     // items をさらにループ処理し、 title と link を抽出
-    for (let { title, link } of items) {
+    // 5章 2-a
+    for (let { title, link, pubDate } of items) {
       // title に文字列 ham という部分文字列が含まれていれば
-      if (title.toLowerCase().includes('chicken')) {
+      // 5章 1-c
+      if (title.toLowerCase().includes(keyword.toLowerCase())) {
         // feedItems 配列に追加
-        feedItems.push({ title, link });
+        // 5章 2-c
+        feedItems.push({ title, link, pubDate });
       }
     }
   }
   // feed配列を返す
   return feedItems;
+};
+
+// 5章 2-b
+// 現在時刻と pubDate の時間差分を返す
+const formatAge = (pubDate) => {
+  // pubDate が空の場合に早期 return
+  // 5章 2-d
+  if (!pubDate) return 'Unknown';
+  const publishedAt = new Date(pubDate);
+  // 無効な日付フォーマットの場合 NaNになる
+  // NaN であれば早期に return
+  // 5章 2-d
+  if (isNaN(publishedAt.getTime())) return 'Unknown';
+  //  現在時刻との差分を計算
+  const diffMs = Date.now() - publishedAt.getTime();
+  // ミリ秒を分に変換
+  const diffMinutes = Math.floor(diffMs / 60000);
+  // 差分が60分未満の場合は、分単位
+  if (diffMinutes < 60) return `${diffMinutes} minutes ago`;
+  // 差分が60分以上の場合は、時間単位
+  const diffHours = Math.floor(diffMinutes / 60);
+  return `${diffHours} hours ago`;
 };
 
 const print = (feedItems) => {
@@ -57,8 +86,15 @@ const print = (feedItems) => {
   if (![title, link].includes(undefined)) customItems.push({ title, link });
   // 標準出力をクリア
   console.clear();
+  //
+  const displayItems = feedItems.concat(customItems).map((item) => ({
+    title: item.title,
+    link: item.link,
+    Age: formatAge(item.pubDate),
+  }));
+
   // feedItems の内容をテーブル形式で出力
-  console.table(feedItems.concat(customItems));
+  console.table(feedItems.concat(displayItems));
   // 最終更新日時をUTC系s機で表示
   console.log('Last updated ', new Date().toUTCString());
 };

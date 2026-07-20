b9f7bc7725dc48dd6c93fc902bea79303c50c01f
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2026-07-20 12:17:48 +0900

    P92 5-3

--- chapter05/index.js
+++ chapter05/index.js
@@ -1,10 +1,19 @@
+import Parser from 'rss-parser';
+
+// Parser クラスをインスタンス化
+const parser = new Parser();
+
 // HTTPリクエストの処理をまとめる非同期関数
 const main = async () => {
   // 指定したURLに対してGETリクエストを行う
   const url = 'https://www.bonappetit.com/feed/recipes-rss-feed/rss';
-  const response = await fetch(url);
-  // レスポンスボディ全体を文字列で読み取り、標準出力する
-  console.log(await response.text());
+  // RSSフィードのCMLを取得解析し、 title と items に分割代入
+  const { title, items } = await parser.parseURL(url);
+  console.log(title); // フィードのタイトルを表示
+  // 項目毎の title,link を抽出
+  const results = items.map(({ title, link }) => ({ title, link }));
+  // テーブル形式で出力
+  console.table(results);
 };
 
 main();

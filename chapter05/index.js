8da6167a62232f37b0e3eda5d393273492c8703c
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2026-07-20 14:55:59 +0900

    P93 取得は3秒間隔で行う

--- chapter05/index.js
+++ chapter05/index.js
@@ -3,17 +3,25 @@ import Parser from 'rss-parser';
 // Parser クラスをインスタンス化
 const parser = new Parser();
 
+// 指定したURLに対してGETリクエストを行う
+const url = 'https://www.bonappetit.com/feed/recipes-rss-feed/rss';
+
 // HTTPリクエストの処理をまとめる非同期関数
 const main = async () => {
-  // 指定したURLに対してGETリクエストを行う
-  const url = 'https://www.bonappetit.com/feed/recipes-rss-feed/rss';
   // RSSフィードのCMLを取得解析し、 title と items に分割代入
   const { title, items } = await parser.parseURL(url);
+
+  // 更新のたびにコンソールをクリアする
+  console.clear(); // a
   console.log(title); // フィードのタイトルを表示
   // 項目毎の title,link を抽出
   const results = items.map(({ title, link }) => ({ title, link }));
   // テーブル形式で出力
   console.table(results);
+
+  // タイムスタンプ表示
+  console.log('Last updated:', new Date().toUTCString());
 };
 
-main();
+// 2秒ごとに取得する
+setInterval(main, 3000);
